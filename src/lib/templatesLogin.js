549577bbf6766416a714e45d1352ca59faac8d61
GiulianaMS <pamelita.suma@gmail.com>
2019-01-29 18:20:19 -0500

    Se realiza funcion para el registro con Facebook

--- src/lib/templatesLogin.js
+++ src/lib/templatesLogin.js
@@ -1,5 +1,6 @@
 import { autentificar } from './autentificacion.js';
 import { logWithGoogle } from './autentificacion.js';
+import { logWithFacebook} from './autentificacion.js';
 
 export const formRegister = `
 
@@ -14,6 +15,7 @@ export const formRegister = `
      </div>
      <div class="">
        <button id="btn-google" class="form" type="submit"><span>Ingresa con Google</span></button>
+       <button id="btn-facebook" class="form" type="submit"><span>Ingresa con Facebook</span></button>
      </div>
    </fieldset>
 
@@ -21,14 +23,16 @@ export const formRegister = `
 
 const formIn = document.getElementById('form-in');
 formIn.innerHTML = formRegister;
-
 let formAutenticacion;
 let btnGoogle;
+let btnFacebook;
 const inicializar = () => {
   formAutenticacion = document.getElementById("form-autenticacion");
   formAutenticacion.addEventListener("submit", autentificar);
   btnGoogle = document.getElementById("btn-google");
   btnGoogle.addEventListener("click", logWithGoogle);
+  btnFacebook = document.getElementById("btn-facebook");
+  btnFacebook.addEventListener("click",logWithFacebook);
 }
 
 window.onload = inicializar;
