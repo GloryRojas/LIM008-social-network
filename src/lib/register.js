a6ce68f43c5bdfe15bfc3e2b59fe31cd8e57a5a4
aurislucero <luro.corilla@gmail.com>
2019-01-29 14:15:01 -0500

    agregando un servidor

--- src/lib/register.js
+++ src/lib/register.js
@@ -1,18 +1,29 @@
 const formRegister = `
 
-    <fieldset>
-      <div>
-        <form id="form-autenticacion" class="" action="index.html" method="post">
-          <h3>Inicia sesión en tu cuenta</h3>
-          <input class="form" type="text" id="email" placeholder="Correo electrónico"></input>
-          <input class="form" type="password" id="password" placeholder="Contraseña"></input>
+    <div>
+      <div class="login">
+      
+      <div class="cabecera1">
+      <img class="logo" src="img/logo.png" alt="">
+       <h1 class="titulo">Infocourse</h1>
+       <h3>Comparte tus conocimientos y aprende de otros</h3>
+       </div>
+       <div class="inicia-sesion">
+      <form id="form-autenticacion" class="" action="index.html" method="post">
+          <h2>Inicia sesión en tu cuenta</h2>
+          <span class="icon-user" ></span><input class="form" type="text" id="email" placeholder="Correo electrónico"></input>
+          <span class="icon-lock-empty" ></span><input class="form" type="password" id="password" placeholder="Contraseña"></input>
           <button class="form" id="btn-login" type="submit"><span>Ingresa</span></button>
+          <button class="form" id="btn-google" type="submit"><span>Iniciar Sesión con Google</span></button>
+        <button class="form" id="btn-facebook" type="submit"><img class = "iconf" src="iconos/facebook.png"><span>Iniciar Sesión con facebook </span></button>
+        <a>¿No tienes cuenta?</a>
+        <a>Regístrate AQUI</a>
         </form>
+        
+        </div>
+        
       </div>
-      <div class="">
-        <button class="form" id="btn-login" type="submit"><span>Ingresa con Google</span></button>
-      </div>
-    </fieldset>
+    </div>
 
 `;
 
@@ -73,5 +84,29 @@ const logWithGoogle = () => {
       // ...
   });
 }
+ingresoGoogle(){
+  if(!firebase.auth.currentUser){
+    const provider= newfirebase.auth.GoogleProvider();
+    provider.addScope('https:www.googleapis.com/auth/plus.login');
+    firebase.auth().signInWithPopup(provider).then ((result)=>{
+const token =result.credential.accessToken;
+const user=result.user;
+    }).catch((error)=>{
+const errorCode= error.code;
+const errorMessage=  error.message;
+    })
+  }
+}
 
 window.onload = inicializar;
+
+/*iniciar con facebook
+authCuentaFacebook(){
+const provider= new firebase.auth.FacebookAuth.provider();
+firebase.auth().signInWithPopup(provider).then(result=>{
+  $('#avatar').attr('src',result.user.photoURL)
+  $('.modal').modal('close')
+  Materialize.toast(``,)
+
+})  
+}*/
