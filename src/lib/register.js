8510401440f02fa26f6dc51d1ed137d1296db4d7
GloryRojas <grojasm94@gmail.com>
2019-01-29 11:23:49 -0500

    Agregando servidor Firebase

--- src/lib/register.js
+++ src/lib/register.js
@@ -1,24 +1,3 @@
-const formRegister = `
-
-    <fieldset>
-      <div>
-        <form id="form-autenticacion" class="" action="index.html" method="post">
-          <h3>Inicia sesión en tu cuenta</h3>
-          <input class="form" type="text" id="email" placeholder="Correo electrónico"></input>
-          <input class="form" type="password" id="password" placeholder="Contraseña"></input>
-          <button class="form" id="btn-login" type="submit"><span>Ingresa</span></button>
-        </form>
-      </div>
-      <div class="">
-        <button class="form" id="btn-login" type="submit"><span>Ingresa con Google</span></button>
-      </div>
-    </fieldset>
-
-`;
-
-const formIn = document.getElementById('form-in');
-formIn.innerHTML = formRegister;
-
 let formAutenticacion;
 const inicializar = () => {
   formAutenticacion = document.getElementById("form-autenticacion");
@@ -74,4 +53,4 @@ const logWithGoogle = () => {
   });
 }
 
-window.onload = inicializar;
+// window.onload = inicializar;
