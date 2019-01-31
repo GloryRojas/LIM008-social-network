d18dbf9dc22950ec890c38a1af4a842b16a7a67c
GloryRojas <grojasm94@gmail.com>
2019-01-31 11:28:30 -0500

    Modificando estrutura de formulario de registro

--- src/lib/templatesLogin.js
+++ src/lib/templatesLogin.js
@@ -22,21 +22,21 @@ export const formLogin = `
 `;
 
 export const formRegister = `
-   <fieldset>
-     <div>
-       <h4>Registrate con tu direccion de correo electronico</h4>
-       <form id="form-registrar" class="" action="index.html" method="post">
-         <input class="form" type="text" id="correo" placeholder="Correo electrónico"></input>
-         <input class="form" type="password" id="contrasena" placeholder="Contraseña"></input>
-         <input class="form" type="password" id="nombres" placeholder="Nombres"></input>
-         <h6>Fecha de Nacimiento</h6>
-         <input class="form" type="text" id="dia" placeholder="Día"></input>
-         <input class="form" type="password" id="mes" placeholder="Mes"></input>
-         <input class="form" type="password" id="año" placeholder="Año"></input>
-         <button class="form" id="btn-register" type="submit"><span>REGISTRATE</span></button>
-       </form>
-     </div>
-   </fieldset>
+   <div>
+    <form id="form-registrar" class="" action="index.html" method="post">
+      <fieldset>
+        <h4>Registrate con tu direccion de correo electronico</h4>
+        <input class="form" type="text" id="correo" placeholder="Correo electrónico"></input>
+        <input class="form" type="password" id="contrasena" placeholder="Contraseña"></input>
+        <input class="form" type="password" id="nombres" placeholder="Nombres"></input>
+        <h6>Fecha de Nacimiento</h6>
+        <input class="form" type="text" id="dia" placeholder="Día"></input>
+        <input class="form" type="password" id="mes" placeholder="Mes"></input>
+        <input class="form" type="password" id="año" placeholder="Año"></input>
+        <button class="form" id="btn-register" type="submit"><span>REGISTRATE</span></button>
+      </fieldset>
+    </form>
+   </div>
 `;
 
 const formIn = document.getElementById('form-in');
