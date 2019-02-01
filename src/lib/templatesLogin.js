ea86eca9fbf50aa50e5410aaaaa0fc9a50072653
GloryRojas <grojasm94@gmail.com>
2019-02-01 11:41:35 -0500

    InnerHTML para template registrar

--- src/lib/templatesLogin.js
+++ src/lib/templatesLogin.js
@@ -1,28 +1,28 @@
 import { logWithPasswordAndEmail, logWithGoogle, logWithFacebook, registerWithEmail } from './autentificacion.js';
 
 export const formLogin = `
-<section>
-  <div class="login">
-    <div class="iniciar-sesion">
-      <div class="cabecera-login">
-        <img class="logo" src="img/logo.png" alt="">
-        <h1 class="titulo">Infocourse</h1>
-        <span class="slogan">Comparte tus conocimientos y aprende de otros</span>
+  <section>
+    <div class="login">
+      <div class="iniciar-sesion">
+        <div class="cabecera-login">
+          <img class="logo" src="img/logo.png" alt="">
+          <h1 class="titulo">Infocourse</h1>
+          <span class="slogan">Comparte tus conocimientos y aprende de otros</span>
+        </div>
+        <form id="form-autenticacion" class="form-autenticacion" action="index.html" method="post">
+          <input class="form" type="text" id="email" placeholder="Correo electrónico"></input>
+          <input class="form" type="password" id="password" placeholder="Contraseña"></input>
+          <button class="btn-login" id="btn-login" type="submit">INICIAR SESIÓN</button>
+          <div class="iconos">
+            <button id="btn-google" class="formGoogle" ><img class = "iconGoogle" src="img/icono-google.png">Iniciar sesión con Google</button>
+            <button id="btn-facebook" class="formFacebook"><img class = "iconFacebook" src="img/icono-face.png">Iniciar sesión con Facebook</button><br><br>
+            <a>¿No tienes cuenta?</a>
+            <a id = "registrate">Regístrate AQUI</a>
+          </div><br>
+        </form>
       </div>
-      <form id="form-autenticacion" class="form-autenticacion" action="index.html" method="post">
-        <input class="form" type="text" id="email" placeholder="Correo electrónico"></input>
-        <input class="form" type="password" id="password" placeholder="Contraseña"></input>
-        <button class="btn-login" id="btn-login" type="submit">INICIAR SESIÓN</button>
-        <div class="iconos">
-          <button id="btn-google" class="formGoogle" ><img class = "iconGoogle" src="img/icono-google.png">Iniciar sesión con Google</button>
-          <button id="btn-facebook" class="formFacebook"><img class = "iconFacebook" src="img/icono-face.png">Iniciar sesión con Facebook</button><br><br>
-          <a>¿No tienes cuenta?</a>
-          <a>Regístrate AQUI</a>
-        </div><br>
-      </form>
     </div>
-  </div>
-</section>
+  </section>
 `;
 
 export const formRegister = `
@@ -36,7 +36,7 @@ export const formRegister = `
         <h6>Fecha de Nacimiento</h6>
         <input class="form" type="text" id="dia" placeholder="Día"></input>
         <input class="form" type="password" id="mes" placeholder="Mes"></input>
-        <input class="form" type="password" id="año" placeholder="Año"></input>
+        <input class="form" type="text" id="año" placeholder="Año"></input>
         <button class="form" id="btn-register" type="submit"><span>REGISTRATE</span></button>
       </fieldset>
     </form>
@@ -46,8 +46,10 @@ export const formRegister = `
 const formIn = document.getElementById('form-in');
 formIn.innerHTML = formLogin;
 
-const formRegisterOne = document.getElementById("form-register");
-formRegisterOne.innerHTML = formRegister;
+const registrate = document.getElementById("registrate");
+registrate.addEventListener("click", () => {
+  formIn.innerHTML = formRegister;
+});
 
 let formAutenticacion;
 let btnGoogle;
